8cc82aa4d3727222bb9b26dff39ce0bd7e1a849b
ElifGokc <elifgokcekoksal@gmail.com>
2026-03-01 11:38:02 +0300

    Ödev 3 1. ve 2. soru

--- src/main/java/com/volkanulutas/javabasicselif/homework/week3/HomeWorkWeek3Q1.java
+++ src/main/java/com/volkanulutas/javabasicselif/homework/week3/HomeWorkWeek3Q1.java
@@ -1,7 +1,90 @@
 package com.volkanulutas.javabasicselif.homework.week3;
 
+import java.util.Scanner;
+
 public class HomeWorkWeek3Q1 {
     public static void main(String[] args) {
-        System.out.println("deneme");
+        Scanner scanner = new Scanner(System.in);
+        double bakiye = 1000;
+        int secim=0;
+
+        do {
+            System.out.println("---------------");
+            System.out.println("1- Bakiye Görüntüle");
+            System.out.println("2- Para Yatır");
+            System.out.println("3- Para Çek");
+            System.out.println("4- Çıkış");
+            System.out.print("Seçiminiz: ");
+
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("Geçersiz giriş! Lütfen sayı giriniz.");
+                scanner.next();
+                continue;
+            }
+
+            secim = scanner.nextInt();
+
+            switch (secim) {
+
+                case 1:
+                    System.out.println("Bakiye: " + bakiye + " TL");
+                    break;
+
+                case 2:
+                    System.out.print("Yatırılacak Tutar: ");
+
+                    if (!scanner.hasNextDouble()) {
+                        System.out.println("Geçersiz tutar!");
+                        scanner.next();
+                        break;
+                    }
+
+                    double yatirilan = scanner.nextDouble();
+
+                    if (yatirilan <= 0) {
+                        System.out.println("Tutar pozitif olmalıdır!");
+                    } else {
+                        bakiye += yatirilan;
+                        System.out.println("Yatırılan Tutar: " + yatirilan + " TL");
+                        System.out.println("Yeni Bakiye: " + bakiye + " TL");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Çekilecek Tutar: ");
+
+                    if (!scanner.hasNextDouble()) {
+                        System.out.println("Geçersiz tutar!");
+                        scanner.next();
+                        break;
+                    }
+
+                    double cekilen = scanner.nextDouble();
+
+                    if (cekilen <= 0) {
+                        System.out.println("Tutar pozitif olmalıdır!");
+                    }
+                    else if (cekilen > bakiye) {
+                        System.out.println("Yetersiz Bakiye!");
+                    }
+                    else {
+                        bakiye -= cekilen;
+                        System.out.println("Çekilen Tutar: " + cekilen + " TL");
+                        System.out.println("Kalan Bakiye: " + bakiye + " TL");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Çıkış yapılıyor...");
+                    break;
+
+                default:
+                    System.out.println("Geçersiz seçim! 1-4 arasında bir değer giriniz.");
+            }
+
+        } while (secim != 4);
+
     }
-}
+    }
+
--- src/main/java/com/volkanulutas/javabasicselif/homework/week3/HomeWorkWeek3Q2.java
+++ src/main/java/com/volkanulutas/javabasicselif/homework/week3/HomeWorkWeek3Q2.java
@@ -1,7 +1,16 @@
 package com.volkanulutas.javabasicselif.homework.week3;
 
+import java.util.Scanner;
+
 public class HomeWorkWeek3Q2 {
     public static void main(String[] args) {
-        System.out.println("deneme");
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        scanner.close();
     }
 }
